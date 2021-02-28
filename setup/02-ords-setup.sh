9457e9cb0531df68a344ba222492718b2df6eae9
Daniel G <dgliddy@gmail.com>
2021-03-01 11:51:43 +1300

    Update 02-ords-setup.sh

--- setup/02-ords-setup.sh
+++ setup/02-ords-setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PARAM_FILE=/opt/oracle/ords/apex-ords-params.properties
+/opt/oracle/product/18c/dbhomeXE/jdk/jre/bin/java -jar /opt/oracle/ords/ords.war configdir /opt/oracle/ords/config
 /opt/oracle/product/18c/dbhomeXE/jdk/jre/bin/java -jar /opt/oracle/ords/ords.war install --silent --parameterFile $PARAM_FILE 
