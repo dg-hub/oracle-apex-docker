45bf1581c3d8f30c4a13bc0ac54a0fc6993a1caf
Daniel Glidden <daniel.glidden@spark.co.nz>
2021-03-01 09:57:20 +1300

    Add ORDS properties

--- setup/01-unzip-ords.sh
+++ setup/01-unzip-ords.sh
@@ -5,3 +5,4 @@ wget https://oraapex.blob.core.windows.net/downloads/$ORDS_FILE
 unzip ./$ORDS_FILE -d /opt/oracle/ords
 rm ./$ORDS_FILE
 cp -r /tmp/apex/images/ /opt/oracle/ords/images
+cp /opt/oracle/scripts/properties/apex-ords-params.properties /opt/oracle/ords/
